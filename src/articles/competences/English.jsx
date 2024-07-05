3c075ad5dbc05a7652d1cd4aa44c32d84b6aa0bd
Idourah Christ <107295034+idourah-christ@users.noreply.github.com>
2024-07-05 13:46:24 +0200

    anglais

--- src/articles/competences/English.jsx
+++ src/articles/competences/English.jsx
@@ -6,70 +6,100 @@ const EnglishArticle = () => {
       <div>
         <h3>Comment j'ai appris l'anglais</h3>
         <p>
-         Une langue est pour moi un scapel qui me permet tel un chirurgient
-         d'inciser la culture et l'histoire d'un peuple pour en decouvrir les entrailles. <br/>
-         Dans un monde que certains qualifient  de village planétaire, parler une langue 
-         étrangère equivaut à s'ouvrir les portes vers de nouvelles horizons. 
-         Je parle courament 6 langues dont 3 langues étrangères. Parmi les 3 langue étrangères, l'anglais est celle avec laquelle 
-         j'ai une rélation particulière. 
+          Une langue est pour moi un scapel qui me permet tel un chirurgient
+          d'inciser la culture et l'histoire d'un peuple pour en découvrir les
+          entrailles. <br />
+          Dans un monde que certains qualifient de village planétaire, parler
+          une langue étrangère équivaut à s'ouvrir les portes vers de nouveaux
+          horizons. Je parle courament 6 langues dont 3 langues étrangères.
+          Parmi les 3 langues étrangères, l'anglais est celle avec laquelle j'ai
+          une rélation particulière.
         </p>
         <p>
-            Issu d'une famille d'adeptes de la "christian science" science chretienne en français, un courant réligieux 
-            originaire des états unis invonté par Mary Baker Eddy. Mary Baker avait écrit et publié plus de 50 ouvrages qui 
-            ont tous été traduit en différentes langues dont le français.
+          Issu d'une famille d'adeptes de la "christian science" science
+          chretienne en français, un courant religieux originaire des Etats-Unis
+          inventé par Mary Baker Eddy. Mary Baker avait écrit et publié plus de
+          50 ouvrages qui ont tous été traduits en différentes langues dont le
+          français.
         </p>
         <p>
-          Mary Baker avait exigé que le text anglais figure toujours à coté de la traduction pour tous ses livres. Par conséquent 
-          tous les livres que j'ai lu, dont elle était l'auteur comportaient pour chaque page le text en anglais à gauche et
-          la version française à droite.
+          Mary Baker avait exigé que le texte anglais figure toujours à coté de
+          la traduction pour tous ses livres. Par conséquent, tous les livres
+          que j'ai lus dont elle était l'auteur comportaient pour chaque page le
+          texte en anglais à gauche et la version française à droite.
         </p>
       </div>
       <div>
         <h4>Mes élements de preuves</h4>
         <p>
-          En 2016 alors que j'était en première année de mes études universitaires, j'ai commencé à animer un club d'anglais avec 
-          un groupe d'étudiant qui voulait comme moi améliorer leur niveau en anglais.
-          Pendant les animations j'étais le MC (Maître de céremonie), j'étais chargé de planifier la séance en sélectionnant différentes
-          activités à l'ordre du jour.
-        </p>
-        <p>Être MC était pour moi un grand défis à relever qui me poussait à la recherche au quotidien. Il me fallait me documenter
-          pour enrichir mon vocabulaire et être capable d'animer la séance pendant 1 heure 30. Au bout de 6 mois je pouvais constater une 
-          amélioration considérable de mon niveau en anglais.
+          En 2016 alors que j'était en première année de mes études
+          universitaires, j'ai commencé à animer un club d'anglais avec un
+          groupe d'étudiants qui voulaient comme moi améliorer leur niveau en
+          anglais. Pendant les animations j'étais le MC maître de cérémonie
+          chargé de planifier la séance en sélectionnant différentes activités à
+          l'ordre du jour.
         </p>
         <p>
-         Une année plus tard j'étais nommé président du club d'anglais de notre université. J'organisais des visites de club d'anglais au sein 
-         d'autre écoles supérieures pour participer à des compétitions et débats en anglais.  
+          Être MC était pour moi un grand défi à relever qui me poussait à la
+          recherche au quotidien. Il me fallait me documenter pour enrichir mon
+          vocabulaire et être capable d'animer la séance pendant 1 heure 30. Au
+          bout de 6 mois je pouvais, constater une amélioration considérable de
+          mon niveau en anglais.
+        </p>
+        <p>
+          Une année plus tard, j'étais nommé président du club d'anglais de
+          notre université. J'organisais des visites de club d'anglais au sein
+          d'autre écoles supérieures pour participer à des compétitions et
+          débats en anglais.
         </p>
       </div>
       <div>
         <h4>En Ukraine</h4>
         <p>
-          En 2018 je suis allé en Ukraine pour poursuivre mes études universitaires, je raconte ce voyage dans cet article <a href="/articles/autonomie">un voyage qui à tout changé</a>.
-          J'ai été capable de faire ce voyage seul grâce à la maîtrise de la langue anglaise.
-          L'anglais m'a aussi permis de vite apprendre le russe en classe préparatoire car les professeurs expliquaient en anglais les règles de grammaire russe et donnaient l'équivalent anglais de chaque mot appris en russe.
+          En 2018, je suis allé en Ukraine pour poursuivre mes études
+          universitaires. Je raconte ce voyage dans cet article{" "}
+          <a href="/articles/autonomie">un voyage qui à tout changé</a>. J'ai
+          été capable de faire ce voyage seul grâce à la maîtrise de la langue
+          anglaise. L'anglais m'a aussi permis de vite apprendre le russe en
+          classe préparatoire car les professeurs expliquaient en anglais les
+          règles de grammaire russe et donnaient l'équivalent anglais de chaque
+          mot appris en russe.
         </p>
         <p>
-          Au bout de 3 mois j'avais le niveau nécessaire pour vivre et étudier dans une ville russophone d'Ukraine comme la ville de Kharkive.
-          Six mois après mon arrivé en Ukraine,  je servais d'interprète à des collègues anglophones et francophones lors des consulations à l'hospital.
+          Au bout de 3 mois j'avais le niveau nécessaire pour vivre et étudier
+          dans une ville russophone d'Ukraine comme la ville de Kharkov. Six
+          mois après mon arrivé en Ukraine, je servais d'interprète à des
+          collègues anglophones et francophones lors des consulations à
+          l'hôspital.
         </p>
       </div>
       <div>
-        <h4>Un dortoire cosmopolitaints</h4>
+        <h4>Un dortoir cosmopolite</h4>
         <p>
-          Pendant les quatres années passées en Ukraine, j'ai partagé le dortoire avec des étudiants originaires des pays d'afrique anglophone et francophone
-          (Afrique du Sud, Gambie, Tanzanie, Cameroun, Nigéria, Ghana, Mali, Sénégal etc..), de l'Inde, du Pakistant, Uzbekistant. 
-          Cette expérience m'a permis d'adapter mon ouïe à différents accents parlés de la langue anglaise.
+          Pendant les quatre années passées en Ukraine, j'ai partagé le dortoir
+          avec des étudiants originaires des pays d'afrique anglophone et
+          francophone (Afrique du Sud, Gambie, Tanzanie, Cameroun, Nigéria,
+          Ghana, Mali, Sénégal etc..), de l'Inde, du Pakistan, Uzbekistan. Cette
+          expérience m'a permis d'adapter mon ouïe à différents accents parlés
+          de la langue anglaise.
         </p>
       </div>
       <div>
-        <h4>Un curcuse universitaires dans la langue de Shakespear</h4>
-        <p>Après avoir terminé la classe préparatoire en langue russe, je me suis inscrit en première année d'un bachelor of sciene en genie logiciel.
-          Le programme a duré 4 ans et était en anglais. N'ayant pas assez de moyen pour acheter des livres, je téléchargais  au format pdf des livres traitans des grandes
-          thématiques du génie logiciel et écris en anglais. 
+        <h4>Un cursuce universitaire dans la langue de Shakespeare</h4>
+        <p>
+          Après avoir terminé la classe préparatoire en langue russe, je me suis
+          inscrit en première année d'un bachelor of science en génie logiciel.
+          Le programme a duré 4 ans et était en anglais. N'ayant pas assez de
+          moyens pour acheter des livres, je téléchargais au format pdf des
+          livres traitants des grandes thématiques du génie logiciel et écrits
+          en anglais.
         </p>
-        <p> En somme la langue anglaise m'a permis de réaliser mon rêve, celui de devenir ingénieur logiciel en brissant la barrière de la langue j'ai accès aux contenus
-          des plus grandes universités comme Oxford, MIT etc.. Au quotidien elle me permet de lire 
-          la documentation des technologies que j'utilise.
+        <p>
+          En somme la langue anglaise m'a permis de réaliser mon rêve, celui de
+          devenir ingénieur logiciel. En brisant la barrière de la langue, j'ai
+          accès aux contenus des plus grandes universités comme Oxford, MIT
+          etc.. Au quotidien elle me permet de lire la documentation des
+          technologies que j'utilise.
         </p>
       </div>
     </Article>
