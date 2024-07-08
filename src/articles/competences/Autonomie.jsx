8d73c277e3d99e1d9425f231148d86c02c0ab1c6
ybahamboulaidourah <yoane.bahamboulaidourah@canal-plus.com>
2024-07-08 12:07:57 +0200

    autonomi upadted

--- src/articles/competences/Autonomie.jsx
+++ src/articles/competences/Autonomie.jsx
@@ -4,11 +4,11 @@ const AutonomieArticle = () => {
   return (
     <Article>
       <div>
-        <h5>Un voyage qui a tout changé</h5>
+        <h3>Un voyage qui a tout changé</h3>
         <p>
-          En avril 2018 je quittai mon pays la République du Congo pour
+          En avril 2018 j'ai quitté mon pays la République du Congo pour
           continuer mes études en Ukraine. Ce voyage était mon premier hors des
-          frontières de mon pays et même du continent. II était claire que
+          frontières du pays et même du continent. II était claire que
           beaucoup de choses changeraient dans ma vie, car pour la première fois
           je m’éloignais du cocon familial.
         </p>
@@ -17,21 +17,20 @@ const AutonomieArticle = () => {
         <p>
           J’avais trouvé en passant par une agence, une inscription en année de
           langue dans une université d’économie de la ville de Kharkiv. L’agence
-          s’occupa de l’achat du billet ainsi que d’autres démarches
+          s’était occupée de l’achat du billet ainsi que d’autres démarches
           administratives nécessaires pour l’obtention d’un visa long séjour en
-          Ukraine. L’agence me rassura que le voyage se passerait bien et qu’il
-          n’y avait rien à craindre. Le jour du voyage arriva, je me rappelle
+          Ukraine. L’agence me rassura que le voyage devrait bien se passer, que je n'avais rien à craindre. 
+          Le jour du voyage, je me rappelle
           encore des derniers instants passés avec les membres de ma famille, ce
-          dernier aurevoir encore vive dans ma mémoire.
+          dernier aurevoir encore vif dans ma mémoire.
         </p>
       </div>
       <div>
         <p>
-          L’avion décolla aux alentours de midi pour Addis Abeba, la capitale de
-          l’Éthiopie. Après 6 heures de vols, nous atterrissions pour une
-          correspondance vers Dubaï. Après quelques minutes d’errances dans
-          l’aéroport je trouvai la porte d’embarquement pour le vol vers Dubaï.
-          Il était 4 heures du matin quand les membres de l’équipage annoncèrent
+          L’avion avait décollé aux alentours de midi pour Addis Abeba. 
+          Après 6 heures de vols l'avion a attéri. Après quelques minutes d’errances dans
+          l’aéroport j'ai trouvé la porte d’embarquement pour le vol vers Dubaï.
+          Il était 4 heure du matin quand les membres de l’équipage ont annoncé
           l’atterrissage à Dubaï. Le prochain décollage pour Kiev était prévu à
           17 heures.
         </p>
@@ -39,110 +38,112 @@ const AutonomieArticle = () => {
       <div>
         <h5>Un cauchemar en plein jour 😃</h5>
         <p>
-          A 16 heures 30 je me présentait à la porte où se trouvait les membres
+          A 16 heure 30 je suis présenté à la porte où se trouvait les membres
           de l’équipage de la compagnie Ukraine air line pour les formalités.
-          <br /> 30 minutes plus tard les membres de l’équipage avait fini
-          d’embarquer et l’avion quitta le tarmac. Billet d’avion à la main
-          gauche, téléphone portable à la main droite, je ne savais que faire.
+          <br /> 30 minutes plus tard ils avaient fini d’embarquer et l’avion quitta le tarmac. 
+          Billet d’avion à la main gauche, téléphone portable à la main droite, je ne savais que faire.
           Tout s’était tellement vite passé que j’avais l’impression de rêver.
           Pourtant s’était réel je n’étais pas dans cet avion qui avait pris son
-          envol pour Kiev. D’un regard presque militaire, il me regarda droit
-          dans les yeux et me dit « we cannot take you with us » Ce qui pourrait
-          se traduire par « Nous ne pouvons pas vous laisser entrer ».
+          envol pour Kiev. En effet quelques minutes plutôt, d’un regard presque militaire un agent de la société Ukraine airline m'avait regardé droit
+          dans les yeux pour me dire « we cannot take you with us » Ce qui pourrait
+          se traduire par « Nous ne pouvons pas vous laisser monter dans l'avion ».
         </p>
         <p>
-          En effet lors de mes formalités, l’équipage avait décidé de ne pas me
-          laisser entrer à cause d’une erreur sur le billet d’avion. Paniqué, je
-          pris mon portable pour appeler ma mère et lui faire part de la
+          Lors de mes formalités, l’équipage avait décidé de ne pas me
+          laisser monter dans l'avion à cause d’une erreur sur le billet d’avion. Paniqué, j'ai
+          pris mon portable pour appeler ma mère et lui faire état de la
           situation. J’avais pu me connecter au réseau wifi de la salle
-          d’attente, ce qui me permis de l’avoir au téléphone.
+          d’attente pour l'appeler.
         </p>
       </div>
       <div>
         <p>
-          Aux environs de 19 heures, elle m’envoya un billet électronique avec
-          un autre itinéraire. Ce n’était plus en vol direct pour Kiev mais
-          Istanbul, capitale de la Turquie. Il était 21 heures 40 quand je me
-          présentais pour les formalités, après 15 minutes d’attentes se fut mon
-          tour. 5 minutes plus tard j’étais dans l’avion de la compagnie Turkish
-          air line, épuisé certes mais surtout rassuré.
+          Aux environs de 19 heures, elle m’a envoyé un billet électronique avec
+          un autre itinéraire. Ce n’était plus un vol direct pour Kiev mais
+          Istanbul. Il était 21 heure quand je me suis présenté pour les formalités, 
+          après 15 minutes d’attente j'ai été reçu par un agent. 5 minutes plus tard j’étais dans l’avion de la compagnie Turkish
+          air line, épuisé certes mais rassuré.
         </p>
       </div>
       <div>
         <h5>Après l'heure ce n'est plus l'heure</h5>
         <p>
-          23 heures 50 minutes, c’est l’heure qu’affichait l’écran de mon
-          téléphone. Cela m’inquiéta car le décollage était prévu à 22 heures,
-          on avait donc pris 1 heure de retard. A minuit, l’avion quitta le
-          tarmac, je pris ma couverture et alla dans les bras de Morphée. A 8
-          heures du matin, l’avion atterris à l’aéroport d’Istanbul.
+          23 heure, c’est l’heure qu’affichait l’écran de mon
+          téléphone. Cela à commencer à m'inquiéter car le décollage était prévu pour 22 heure.
+          On avait pris 1 heure de retard. A minuit, l’avion avait quitté le
+          tarmac, j'ai pris ma couverture, je me suis endormi quelque minutes plus tard. A 8
+          heure du matin, l’avion a atterri à l’aéroport d’Istanbul.
         </p>
         <p>
-          Sans perdre le temps je me dirigeai vers un bureau provisoire de la
-          compagnie Turkish air line. A mon arrivée, j’ai présenté mon billet et
-          demander qu’on m’indique la porte pour le prochain vol à destination
-          de Kiev. Après quelques minutes de vérification, il me remit mon
-          téléphone et me répondu qu’il n’avait aucun vol à destination de Kiev
+          Sans perdre du temps je me suis dirigé vers les bureaux de la
+          compagnie Turkish air line. A mon arrivée, j’ai présenté mon billet en
+          demandant qu’on m’indique la porte pour le prochain vol à destination
+          de Kiev. Après quelques minutes de vérification, l'agant me remit mon
+          téléphone en répondant qu’il n’y avait aucun vol à destination de Kiev
           et que le dernier venait de décoller 30 minutes avant mon arrivée.
         </p>
       </div>
       <div>
         <p>
-          Je retorqua que ce n’était pas ma faute car l’avion venait d’atterrir
+          Je retorquais que ce n’était pas ma faute car l’avion venait d’atterrir
           et que par conséquent le tort revenait à la compagnie. J’avais très
-          vite réalisé que je n’étais pas le seul dans cette situation car il
-          y’avait autour de moi une dizaine de personnes demandant un potentiel
-          remboursement.
+          vite réalisé que je n’étais pas le seul dans cette situation,  il
+          y’avait autour de moi une dizaine de personnes qui demandaient à  se faire
+          rembourser.
         </p>
       </div>
       <div>
         <h5>La tension au bout de fil</h5>
         <p>
-          Au bout de quelques minutes la tension monta d’un cran, les gens
+          Au bout de quelques minutes la tension était monté d’un cran, les gens
           commençaient à perdre patience. Dans un coin à l’écart, j’avais
-          remarqué la présence d’un jeune d’africain qui m’avait l’air bien
-          triste. D’un air presque amical je l’avais salué en anglais pour
-          commencer et demanda ses origines. Il était originaire du Nigéria et
+          remarqué la présence d’un jeune africain qui m’avait l’air bien
+          triste. D’un air amical je l’avais salué en anglais pour
+          en demandant d'où il venait. Il était originaire du Nigéria et
           se rendait en Ukraine pour ses études.
         </p>
       </div>
       <p>
         3 jours plus tôt il n’avait pas pu retrouver la porte d’embarquement à
-        temps et avait par conséquent raté son vol. La compagnie Turkish air
+        temps et avait raté son vol. La compagnie Turkish air
         line lui demanda 400 $ pour un nouveau billet à destination d’Ukraine.
-        N’ayant pas d’argent sur lui, il avait passé 3 nuits à l’aéroport
+        N’ayant pas d’argent sur lui, le jeune avait passé 3 nuits à l’aéroport
         d’Istanbul tout en essayant de joindre ses parents.
       </p>
 
       <div>
         <h5>La diplomatie, une arme redoutable</h5>
         <p>
-          D’un geste de la main, il me fit signe de venir. C’était un employé de
-          la compagnie Turkish air line qui demandait à me parler. Je m’étais
-          rapproché interrompant ainsi ma conversation avec le jeune nigérien.
-          Il disait que la compagnie avait pu trouver un vol à destination
-          d’Ukraine mais que le décollage était pour 17 heures. J’avais répondu
-          que j’étais d’accord à l’idée d’attendre jusqu’à 17 heures à condition
+          D’un geste de la main l'agent me fit signe de venir. Interrompant ma conversation 
+          avec le jeune homme, je me suis rapproché.
+          l'agent me dit que la compagnie avait pu trouver un vol à destination
+          d’Ukraine mais le décollage était pour 17 heure. J’ai répondu
+          que j’étais d’accord à l’idée d’attendre jusqu’à 17 heure à condition
           d’avoir une connexion internet pour signaler ce changement de
           programme à ceux qui m’attendaient en Ukraine. Aux environs de 16
-          heurs, je quittai le « Resting room » une salle de repos pour
-          voyageurs.
+          heur, j'ai quitté le « Resting room » une salle de repos pour
+          voyageur.
         </p>
         <p>
           2 heures plutôt j’avais utilisé un bon de gratuité donné par un
-          employé de Turkish air line pour me payer un repas dans un fastfood de
-          l’aéroport. Après une recherche qui dura au moins 40 minutes, j’avais
-          enfin retrouvé la porte d’embarquement. Cet exercice m’avais fait
-          comprendre pourquoi beaucoup de voyageurs à l’instar du jeune nigérien
-          rataient leur vol Il était 18 heures sur mon portable. Les formalités
-          s’étaient bien passées, je m’étais confortablement installé sur mon
-          siège et attendais le décollage. Après 2 heures de vol nous étions à
-          Kiev. Il me fallait maintenant trouver un billet de bus pour me rendre
-          à Kharkiv. Aux environs de 22 heures je me trouvais dans le bus pour
-          Kharkiv et en avais pour au moins 8 heures de routes. Après quelques
-          minutes de vérification le bus avait démarré. C’était le début
-          tumultueux d’une belle aventure.
+          employé de Turkish air line pour m'offrir un repas dans un fastfood de
+          l’aéroport. 
+          
+          Après une recherche qui dura au moins 40 minutes, j’ai
+          enfin retrouvé la porte d’embarquement. Cet exercice m’a fait
+          comprendre le pourquoi beaucoup de voyageurs à l’instar du jeune nigérien
+          rataient leur vol. 
         </p>
+        <p>
+          Il était 18 heure sur mon portable. Les formalités
+          s’étant bien passées, je me suis confortablement installé sur mon
+          siège et attendais le décollage. Après 2 heure de vol l'avion atterri à
+          Kiev. Il me fallait maintenant trouver un billet de bus pour me rendre
+          à Kharkiv. Aux environs de 22 heure je m'étais dans le bus pour
+          Kharkiv. J'en avais au moins pour 8 heures de routes. Après quelques
+          minutes de vérification le bus a démarré. C’était le début
+          tumultueux d’une belle aventure.
+          </p>
       </div>
     </Article>
   );
